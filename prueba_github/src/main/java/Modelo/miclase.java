e174491b1759f2d4af75791207a33da3b7bb506d
sandr <sandr@Sandra>
2023-12-06 19:14:51 +0100

    interfaz y clase

---
+++ prueba_github/src/main/java/Modelo/miclase.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+import interfaces.Interfaz;
+
+public class miclase implements Interfaz {
+
+	
+
+	@Override
+	public int metododevuelveentero() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
